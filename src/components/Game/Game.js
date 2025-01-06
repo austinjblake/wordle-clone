1ae0a5a3f918fd13e2f77d678bd322c16bbedb60
Austin Blake <ajblake2189@gmail.com>
2025-01-06 16:05:33 -0600

    ex 4

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,6 +5,7 @@ import { WORDS } from '../../data';
 import GuessInput from '../GuessInput';
 import GuessList from '../GuessList';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants.js';
+import { checkGuess } from '../../game-helpers.js';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -16,7 +17,7 @@ function Game() {
 	const [guessList, setGuessList] = useState(() => {
 		const guessAttempts = new Array(NUM_OF_GUESSES_ALLOWED).fill();
 		guessAttempts.forEach((attempt, i) => {
-			guessAttempts[i] = { word: '', id: crypto.randomUUID() };
+			guessAttempts[i] = { word: '', id: crypto.randomUUID(), status: null };
 		});
 		return guessAttempts;
 	});
@@ -27,7 +28,12 @@ function Game() {
 		if (currentGuess >= NUM_OF_GUESSES_ALLOWED) {
 			return;
 		}
-		const newGuess = { word: guess, id: crypto.randomUUID() };
+		const guessStatus = checkGuess(guess, answer);
+		const newGuess = {
+			word: guess,
+			id: crypto.randomUUID(),
+			status: guessStatus,
+		};
 		const newGuessList = [...guessList];
 		newGuessList[currentGuess] = newGuess;
 		setGuessList(newGuessList);
--- src/components/GuessList/GuessList.js
+++ src/components/GuessList/GuessList.js
@@ -5,11 +5,21 @@ function GuessList({ guessList }) {
 		<div className='guess-results'>
 			{guessList.map((guess, i) => (
 				<p key={guess.id} className='guess'>
-					<span className='cell'>{guess.word[0]}</span>
-					<span className='cell'>{guess.word[1]}</span>
-					<span className='cell'>{guess.word[2]}</span>
-					<span className='cell'>{guess.word[3]}</span>
-					<span className='cell'>{guess.word[4]}</span>
+					<span className={`cell ${guess.status && guess.status[0].status}`}>
+						{guess.word[0]}
+					</span>
+					<span className={`cell ${guess.status && guess.status[1].status}`}>
+						{guess.word[1]}
+					</span>
+					<span className={`cell ${guess.status && guess.status[2].status}`}>
+						{guess.word[2]}
+					</span>
+					<span className={`cell ${guess.status && guess.status[3].status}`}>
+						{guess.word[3]}
+					</span>
+					<span className={`cell ${guess.status && guess.status[4].status}`}>
+						{guess.word[4]}
+					</span>
 				</p>
 			))}
 		</div>
